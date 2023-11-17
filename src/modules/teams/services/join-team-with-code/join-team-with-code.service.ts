dc75745757788dd44a96e4be46abb4f9b322f172
mechztcc <dojathan@gmail.com>
2023-11-17 06:36:23 -0500

    feat: add some validations on join team with code

--- src/modules/teams/services/join-team-with-code/join-team-with-code.service.ts
+++ src/modules/teams/services/join-team-with-code/join-team-with-code.service.ts
@@ -1,11 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/modules/prisma/PrismaClient';
+
+interface IRequest {
+  userId: number;
+  code: string;
+}
 
 @Injectable()
 export class JoinTeamWithCodeService {
   constructor(private prisma: PrismaService) {}
 
-  execute(code: string): Promise<any> {
-    return;
+  async execute(payload: IRequest): Promise<any> {
+    const inviteCodeExists = await this.prisma.invitationCode.findUnique({
+      where: { code: payload.code },
+    });
+
+    if (!inviteCodeExists) {
+      throw new NotFoundException('Provided invitation code is invalid');
+    }
+
+    const user = await this.prisma.user.findUnique({
+      where: { id: payload.userId },
+    });
+
+    if (user.role !== 'STUDENT') {
+      throw new UnprocessableEntityException(
+        'Only students can join a team using an invitation code',
+      );
+    }
+
+    return await this.prisma.usersOnTeams.create({
+      data: { teamId: inviteCodeExists.teamId, userId: payload.userId },
+    });
   }
 }
